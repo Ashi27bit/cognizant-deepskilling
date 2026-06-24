fb462213286f8a4c442b4473f8b273b49859da71
Ashi27bit <ashikasunil1@gmail.com>
2026-06-24 22:41:46 +0530

    Create MVCTest.java

---
+++ DesignPatterns/MVCPatternExample/MVCTest.java
@@ -0,0 +1,27 @@
+public class MVCTest {
+
+    public static void main(String[] args) {
+
+        Student student =
+                new Student(
+                        "Ashika",
+                        101,
+                        "A");
+
+        StudentView view =
+                new StudentView();
+
+        StudentController controller =
+                new StudentController(
+                        student,
+                        view);
+
+        controller.updateView();
+
+        System.out.println();
+
+        controller.setStudentGrade("A+");
+
+        controller.updateView();
+    }
+}
