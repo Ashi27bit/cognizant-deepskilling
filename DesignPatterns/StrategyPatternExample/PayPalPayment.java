d95382e95be01f296a2130351d5be0a4cb7ada30
Ashi27bit <ashikasunil1@gmail.com>
2026-06-24 22:36:52 +0530

    Create PayPalPayment.java

---
+++ DesignPatterns/StrategyPatternExample/PayPalPayment.java
@@ -0,0 +1,10 @@
+public class PayPalPayment
+        implements PaymentStrategy {
+
+    public void pay(double amount) {
+
+        System.out.println(
+                "Paid " + amount +
+                        " using PayPal");
+    }
+}
